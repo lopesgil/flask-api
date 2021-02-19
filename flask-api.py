3b17f97c354f79742c952818cccd75ebe58521ef
Gilberto Lopes <gilbertolif@dcc.ufrj.br>
2021-02-19 03:59:38 -0300

    (add) models e migrations de usuário e país

--- flask-api.py
+++ flask-api.py
@@ -1,1 +1,6 @@
-from quotr import app
+from quotr import app,db
+from quotr.models import User, Country
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Country': Country}
