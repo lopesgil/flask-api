e7b09bc270d6d77d42a3190c6596f20dcbed06d5
Gilberto Lopes <gilbertolif@dcc.ufrj.br>
2021-02-19 17:28:37 -0300

    (add) cruds e rotas de usuários e países

--- quotr/routes.py
+++ quotr/routes.py
@@ -1,6 +1,10 @@
-from quotr import app
+from quotr import app, manager
+from quotr.models import Country, User
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
+
+manager.create_api(Country, methods=['GET', 'POST', 'PUT', 'DELETE'])
+manager.create_api(User, methods=['GET', 'POST', 'PUT', 'DELETE'])
