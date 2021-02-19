d0fc88bc065e3a8fa0ef5e310faf690c5c206220
Gilberto Lopes <gilbertolif@dcc.ufrj.br>
2021-02-19 19:14:56 -0300

    (add) model e migration de citações

--- quotr/models.py
+++ quotr/models.py
@@ -15,6 +15,16 @@ class User(db.Model):
     email = db.Column(db.String(120),index=True, unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
     country_id = db.Column(db.Integer, db.ForeignKey('country.id'))
+    quotes = db.relationship('FavoriteQuote', backref='user', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+class FavoriteQuote(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(128), index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return '<Quote "{}">'.format(self.body)
